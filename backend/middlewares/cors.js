101e92eab6fd39ed550fcb85172e081f02f0f079
Denis Panyushin <Panyushin.D@yandex.ru>
2022-08-27 17:01:32 +0300

    Добавил фронтенд и бэк

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -11,7 +11,7 @@ module.exports = ((req, res, next) => {
 
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Credentials', true);
   }
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', ALLOWED_METHODS);
