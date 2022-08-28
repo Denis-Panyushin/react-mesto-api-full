2e1bd2d2ad78a2243d15491cd72a06e6c1984b29
Denis Panyushin <Panyushin.D@yandex.ru>
2022-08-28 15:38:57 +0300

    Добавил фронтенд и бэк

--- backend/controllers/cards.js
+++ backend/controllers/cards.js
@@ -14,7 +14,9 @@ module.exports.getCards = (req, res, next) => {
 module.exports.createCard = (req, res, next) => {
   const { name, link } = req.body;
 
-  Card.create({ name, link, owner: req.user._id })
+  Card.create({
+    name, link, owner: req.user._id, likes: {},
+  })
     .then((card) => res.send({ card }))
     .catch((err) => {
       if (err.name === 'ValidationError') {
